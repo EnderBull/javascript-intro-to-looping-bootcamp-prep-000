3ed82f0a7b7ffe1f44e9714db284805d63af1227
Simon A Bull <simonbull1@gmail.com>
2018-07-25 14:31:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -30,7 +30,7 @@ return Math.random() >= 0.5
 function doWhileLoop(array){
   do {
     array.pop();
-  } while (array.length > 0 && maybeTrue())
+  } while (array.length > 0 || maybeTrue())
   
   return array
 }
